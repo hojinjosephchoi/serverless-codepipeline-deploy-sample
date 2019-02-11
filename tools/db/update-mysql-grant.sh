88dfc79d1a3905074c8e5f1d0cc1901c7eaf28f6
hojinjosephchoi <escape3rd@gmail.com>
2019-02-12 00:00:12 +0900

    change to install state

--- tools/db/update-mysql-grant.sh
+++ tools/db/update-mysql-grant.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SELF="${BASH_SOURCE[0]}"
 cd `dirname "$SELF"`
-sudo docker-compose -p lululala exec -T mysql sh /tools/update-grant.sh
+docker-compose -p lululala exec -T mysql sh /tools/update-grant.sh
